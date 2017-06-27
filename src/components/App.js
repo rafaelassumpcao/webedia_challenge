636910334b37f3712b79555414a2b6387228ba94
rafaelAssumpcao <rafaelfassumpcao@gmail.com>
2017-06-27 19:58:15 -0300

    entry point to the front application

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
 import React, { Component} from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import Header from './Header';
+import Mobile from './Mobile';
+import Desktop from './Desktop';
+import BlogPostForm from './BlogPostForm';
 
-
-//import Menu from 'react-burger-menu/lib/menus/push'
+import { ResponsiveComponent }  from 'react-responsive-component';
 
 export default class App extends Component {
+
   render(){
     return (
-      <Header>
-         <p>Um Site do <span className="logo">Web<i>edia</i></span><i className="trademark">&trade;</i> Group</p>
-      </Header>  
+      <div>
+        <Desktop />
+        <Mobile />     
+      </div>
     );
   }
   
